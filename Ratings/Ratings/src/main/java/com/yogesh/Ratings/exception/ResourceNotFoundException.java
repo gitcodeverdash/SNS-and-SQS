34b090305ec41d559b9a0fefb05387c86856472d
gitcodeverdash <154234922+gitcodeverdash@users.noreply.github.com>
2025-06-22 01:27:22 +0530

    Delete Ratings directory

--- Ratings/Ratings/src/main/java/com/yogesh/Ratings/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.yogesh.Ratings.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String provideCorrectRatingId) {
-        super(provideCorrectRatingId);
-    }
-
-}

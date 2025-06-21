2adad4c7d798d19fae831680ba3b8f9ef3dbbdcf
gitcodeverdash <154234922+gitcodeverdash@users.noreply.github.com>
2025-06-22 01:27:34 +0530

    Delete UserServicesDemo directory

--- UserServicesDemo/UserServicesDemo/src/main/java/com/yogesh/UserServices/repositories/UserRepositories.java
+++
@@ -1,8 +0,0 @@
-package com.yogesh.UserServices.repositories;
-
-
-import com.yogesh.UserServices.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositories extends JpaRepository<User, String> {
-}
